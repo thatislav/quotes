28a5b4d74aecad766c654508b117769bd95e8398
thatislove <thatislove@ya.ru>
2019-10-13 19:59:31 +0300

    Continue quotescraper

--- quotescraper.py
+++ quotescraper.py
@@ -5,17 +5,46 @@ import json.decoder
 import requests
 from lxml import html
 from datetime import datetime, timedelta
+from itertools import groupby
 
+
+article = {'id': '',
+           'date': '',
+           'body': '',
+           'rating': ''
+           }
 
 
 def make_request():
     response = requests.request('GET', 'https://bash.im/')
     parsed = html.fromstring(response.text)
-    articles_from_html = parsed.xpath('//article[@class="quote"]')
     # ('//*[@id="departure-city"]/option[@value]/text()')
-    
+    return parsed
+
+
+def get_articles_from_page(parsed):
+    articles_from_html = parsed.xpath('//article[@class="quote"]')
+    articles_ready = []
+    for ar in articles_from_html:
+        article_ready = get_article(ar)
+        articles_ready.append(article_ready)
+    return articles_ready
+
+
+def get_article(article_from_html, *args, **kwargs):
+    id = article_from_html.xpath('string(//@data-quote)')
+    raw_date = article_from_html.xpath('.//div[@class="quote__header_date"]/text()')
+    datetime_act = raw_date[0].split('в')
+    datetime_act_1 = [part.strip() for part in datetime_act]
+    test = str(raw_date[0])
+    datetime_sec = re.match(r'(\d{1,2}\.\d{1,2}\.\d{4}).+(\d{1,2}\:\d{2})', test)
+    datetime_third = str(raw_date[0]).replace('в', '').strip()
     input()
 
 
-make_request()
+def master_def():
+    parsed = make_request()
+    articles = get_articles_from_page(parsed)
 
+
+master_def()
