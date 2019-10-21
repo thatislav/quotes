111e7b441d91414cfd92951dc7dbe732ffba4ef9
thatislove <thatislove@ya.ru>
2019-10-21 05:33:13 +0300

    Add more changes

--- DBcm.py
+++ DBcm.py
@@ -7,76 +7,87 @@ from page_getter import count_pages
 import sqlite3
 
 
-pages = count_pages()
-conn = sqlite3.connect('QuotesDB.db')
-cur = conn.cursor()
+class DBfiller:
 
+    def __init__(self):
+        self.pages = count_pages()
+        self.conn = sqlite3.connect('QuotesDB.db')
+        self.cur = self.conn.cursor()
+        self.create_table_quotes()
 
-def create_table_quotes():
-    try:
-        _SQL_create_table_quotes = """CREATE TABLE QUOTES
-                                    ([quote_id] integer PRIMARY KEY,
-                                    [quote_date] date,
-                                    [quote_text] text,
-                                    [quote_rate] integer)
-                                """
-        cur.execute(_SQL_create_table_quotes)
-        conn.commit()
-    except sqlite3.OperationalError as err:
-        print('\nWARNING: Trying to create table QUOTES, getting:\n"', err, '"')
-
-
-def scrape_all_quotes(pages_quantity):
-    for page_number in range(1, pages_quantity+1):
-        quotes_from_single_page = scrape_quotes_from_page(page_number)
-        add_quotes_to_db(quotes_from_single_page)
-
-
-def add_quotes_to_db(quotes):
-    # existing_ids = 0
-    for quote in quotes:
-        _SQL_insert_quotes = """INSERT INTO QUOTES 
-                                (quote_id, quote_date, quote_text, quote_rate)
-                                VALUES
-                                ({id}, '{date}', '{text}', {rating})
-                            """.format(**quote)
+    def create_table_quotes(self):
         try:
-            cur.execute(_SQL_insert_quotes)
+            _SQL_create_table_quotes = """CREATE TABLE QUOTES
+                                        ([quote_id] integer PRIMARY KEY,
+                                        [quote_date] date,
+                                        [quote_text] text,
+                                        [quote_rate] integer)
+                                    """
+            self.cur.execute(_SQL_create_table_quotes)
+            self.conn.commit()
+        except sqlite3.OperationalError as err:
+            print('\nWARNING: Trying to create table QUOTES, getting:\n"', err, '"')
+
+    def scrape_all_quotes(self):
+        for page_number in range(1, self.pages+1):
+            quotes_from_single_page = scrape_quotes_from_page(page_number)
+            self.add_quotes_to_db(quotes_from_single_page)
+
+    def add_quotes_to_db(self, quotes):
+        # existing_ids = 0
+        for quote in quotes:
+            _SQL_insert_quotes = """INSERT INTO QUOTES 
+                                    (quote_id, quote_date, quote_text, quote_rate)
+                                    VALUES
+                                    ({id}, '{date}', '{text}', {rating})
+                                """.format(**quote)
+            try:
+                self.cur.execute(_SQL_insert_quotes)
+            except Exception as err:
+                continue
+        self.conn.commit()
+        #         existing_ids += 1
+        # if existing_ids > 0:
+        #     print('\nWARNING:', existing_ids, 'quotes we just trying to add are already exists.\n')
+
+    def count_quotes_in_db(self):
+        _SQL_select_quotes = """
+                                --SELECT quote_id, quote_date, '', '', ''
+                                --FROM QUOTES
+                                --WHERE date(quote_date) = '2019-10-20'
+                                --UNION
+                                SELECT '','','====================', 'QUOTES TOTAL in DB: ', count(quote_id)
+                                FROM QUOTES
+                            """
+        try:
+            self.cur.execute(_SQL_select_quotes)
+            for item in self.cur.fetchall():
+                for i in item:
+                    print(i)
         except Exception as err:
-            continue
-    conn.commit()
-    #         existing_ids += 1
-    # if existing_ids > 0:
-    #     print('\nWARNING:', existing_ids, 'quotes we just trying to add are already exists.\n')
+            print('\nWARNING: Something went wrong in "select_quotes":\n"', err, '"')
 
+    def select_quote_by_id(self, id):
+        _SQL_select_quote_by_id = """SELECT *
+                                    FROM QUOTES
+                                    WHERE quote_id = {}""".format(id)
+        try:
+            self.cur.execute(_SQL_select_quote_by_id)
+            for item in self.cur.fetchall():
+                for i in item:
+                    print(i)
+            acceptable_form_for_flask = tuple(self.cur.fetchall())
+            return acceptable_form_for_flask
+        except Exception as err:
+            print('\nWARNING: Something went wrong in "select_quote_by_id":\n"', err, '"')
 
-def count_quotes_in_db():
-    _SQL_select_quotes = """
-                            --SELECT quote_id, quote_date, '', '', ''
-                            --FROM QUOTES
-                            --WHERE date(quote_date) = '2019-10-20'
-                            --UNION
-                            SELECT '','','====================', 'QUOTES TOTAL in DB: ', count(quote_id)
-                            FROM QUOTES
-                        """
-    try:
-        cur.execute(_SQL_select_quotes)
-        for item in cur.fetchall():
-            for i in item:
-                print(i)
-    except Exception as err:
-        print('\nWARNING: Something went wrong in "select_quotes":\n"', err, '"')
+    def finish_work(self):
+        self.conn.close()
 
-
-def mainer():
-    create_table_quotes()
-    scrape_all_quotes(pages)
-    # count_quotes_in_db()
-
-
-# mainer()
-
-conn.close()
+    # def quotes_miner():
+    #     create_table_quotes()
+    #     scrape_all_quotes(pages)
+        # count_quotes_in_db()
 
 
 class UseDatabase:
--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 The server.
 """
 from flask import Flask, render_template, request, escape, session, copy_current_request_context, abort
-# from DBcm import UseDatabase
+from DBcm import DBfiller
 from quotescraper import scrape_quotes_from_page
 from threading import Thread
 
@@ -15,23 +15,45 @@ quotes = scrape_quotes_from_page(1)
 def entry_page() -> 'html':
     """Renders HTML-form"""
     return render_template('entry.html',
-                           the_title='Welcome to Bash_quotes!')
+                           the_title='Welcome to Bash_quotes! ;)')
 
 
 @app.route('/all_quotes')
 def show_all_quotes():
     return render_template('viewallquotes.html',
-                           the_title='ВСЕ ЦИТАТЫ С БАША В ОДНОМ МЕСТЕ, ААААА!!!!!')
+                           the_title='ALL QUOTES FROM BASH IN SINGLE PLACE, AAAAA!!!!!')
 
 
-@app.route('/quote/<int:quote_id_on_html>')
-def show_quote_by_id(quote_id_on_html):
+@app.route('/all_quotes/scrape')
+def fill_db_with_quotes():
+    filler = DBfiller()
+    filler.scrape_all_quotes()
+    return render_template('quotes_are_scraped.html',
+                           the_title="You're the most patient person! RESPECT!")
 
-    quote_id_from_form = request.values.get('quote_id_on_html')
-    if quote_id_from_form < 1 or quote_id_from_form > 1000000:
-        quote_id_from_form = 1
 
-    return '<h1>Цитаты из show_quote_by_id: <small>{}</small></h1>'.format(quote_id_from_form)
+# @app.route('/quote<int:quote_id_on_html>')
+@app.route('/quote')
+# def show_quote_by_id(quote_id_on_html):
+def show_quote_by_id():
+
+    quote_id = request.args.get('quote_id_on_html', None)
+    try:
+        quote_id = int(quote_id)
+    except Exception as err:
+        quote_id = 1
+    if quote_id < 1 or quote_id > 1000000:
+        quote_id = 1
+    print('quote_id = ', quote_id)
+
+    try:
+        selecter = DBfiller()
+        finded_quote = selecter.select_quote_by_id(quote_id)
+        return finded_quote
+    except Exception as err:
+        return 'No such quote.. Think about it.'
+
+    return '<h1>Цитата из show_quote_by_id: <small>{}</small></h1>'.format(quote_id)
     # quote_to_show = [quote for quote in quotes if quote['id'] == quote_id]
     # if len(quote_to_show) == 1:
     #     result = '<h1>Цитата № {id}:</h1><p><b>{text}</h3></b><p>Дата: {date}</p><p>Рейтинг: {rating}</p>'
